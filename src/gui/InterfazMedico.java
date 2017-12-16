bf9d53450101a1862138adfd722457cb569b7adb
Carlos Fernando Tovar Ceron <carlos.fernando.tovar@correounivalle.edu.co>
2017-12-16 02:11:39 -0500

    Interfaz Cama, DAOCama, ControladorCama

--- src/gui/InterfazMedico.java
+++ src/gui/InterfazMedico.java
@@ -3,18 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI;
+package gui;
 
 /**
  *
  * @author jdtorres
  */
-public class InterfazEnfermera extends javax.swing.JFrame {
+public class InterfazMedico extends javax.swing.JFrame {
 
     /**
-     * Creates new form InterfazEnfermera
+     * Creates new form InterfazMedico
      */
-    public InterfazEnfermera() {
+    public InterfazMedico() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class InterfazEnfermera extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InterfazEnfermera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InterfazMedico.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InterfazEnfermera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InterfazMedico.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InterfazEnfermera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InterfazMedico.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(InterfazEnfermera.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InterfazMedico.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new InterfazEnfermera().setVisible(true);
+                new InterfazMedico().setVisible(true);
             }
         });
     }
