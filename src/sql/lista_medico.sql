bcbd2c4daacb95b5661ea92609627d46926f3bd8
krypt <krypth@gmail.com>
2017-12-16 22:19:44 -0500

    inicio medicos

---
+++ src/sql/lista_medico.sql
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  juan
+ * Created: 16-dic-2017
+ */
+
+SELECT * FROM medico;
